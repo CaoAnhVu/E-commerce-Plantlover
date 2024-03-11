254d1120cd076831bee0186b15ceb4910e71d978
Andyvux <anhvuktnh@gmail.com>
2024-03-11 10:23:40 +0700

    Initial commit

---
+++ Controllers/GiaoDien.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cs_Plantlover.Controllers
+{
+    public class GiaoDien : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
